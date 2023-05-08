545f375d889820454be7558ad51be899a6756a89
xisforxerxes <ouellr@gmail.com>
2023-05-07 22:38:51 -0400

    More cleanup around caching and la

--- lib/cache.sh
+++ lib/cache.sh
@@ -31,68 +31,18 @@ get_cache_status() {
   fi
 }
 
-get_cache_directories() {
-  local build_dir="$1"
-  local dirs1 dirs2
-  dirs1=$(read_json "$build_dir/package.json" ".cacheDirectories | .[]?")
-  dirs2=$(read_json "$build_dir/package.json" ".cache_directories | .[]?")
-
-  if [ -n "$dirs1" ]; then
-    echo "$dirs1"
-  else
-    echo "$dirs2"
-  fi
-}
-
 restore_default_cache_directories() {
   local build_dir=${1:-}
   local cache_dir=${2:-}
-  local npm_cache=${4:-}
 
-  if [[ "$USE_NPM_INSTALL" == "false" ]]; then
-    if [[ -d "$cache_dir/node/cache/npm" ]]; then
-      rm -rf "$npm_cache"
-      mv "$cache_dir/node/cache/npm" "$npm_cache"
-      echo "- npm cache"
-    else
-      echo "- npm cache (not cached - skipping)"
-    fi
+  # node_modules
+  if [[ -e "$cache_dir/node/cache/node_modules" ]]; then
+    echo "- node_modules"
+    mkdir -p "$(dirname "$build_dir/node_modules")"
+    mv "$cache_dir/node/cache/node_modules" "$build_dir/node_modules"
   else
-    # node_modules
-    if [[ -e "$build_dir/node_modules" ]]; then
-      echo "- node_modules is checked into source control and cannot be cached"
-    elif [[ -e "$cache_dir/node/cache/node_modules" ]]; then
-      echo "- node_modules"
-      mkdir -p "$(dirname "$build_dir/node_modules")"
-      mv "$cache_dir/node/cache/node_modules" "$build_dir/node_modules"
-    else
-      echo "- node_modules (not cached - skipping)"
-    fi
+    echo "- node_modules (not cached - skipping)"
   fi
-}
-
-restore_custom_cache_directories() {
-  local cache_directories
-  local build_dir=${1:-}
-  local cache_dir=${2:-}
-  # Parse the input string with multiple lines: "a\nb\nc" into an array
-  mapfile -t cache_directories <<< "$3"
-
-  echo "Loading ${#cache_directories[@]} from cacheDirectories (package.json):"
-
-  for cachepath in "${cache_directories[@]}"; do
-    if [ -e "$build_dir/$cachepath" ]; then
-      echo "- $cachepath (exists - skipping)"
-    else
-      if [ -e "$cache_dir/node/cache/$cachepath" ]; then
-        echo "- $cachepath"
-        mkdir -p "$(dirname "$build_dir/$cachepath")"
-        mv "$cache_dir/node/cache/$cachepath" "$build_dir/$cachepath"
-      else
-        echo "- $cachepath (not cached - skipping)"
-      fi
-    fi
-  done
 }
 
 clear_cache() {
@@ -105,44 +55,14 @@ clear_cache() {
 save_default_cache_directories() {
   local build_dir=${1:-}
   local cache_dir=${2:-}
-  local npm_cache=${4:-}
 
-  if [[ "$USE_NPM_INSTALL" == "false" ]]; then
-    if [[ -d "$npm_cache" ]]; then
-      mv "$npm_cache" "$cache_dir/node/cache/npm"
-      echo "- npm cache"
-    else
-      echo "- npm cache (nothing to cache)"
-    fi
+  # node_modules
+  if [[ -e "$build_dir/node_modules" ]]; then
+    echo "- node_modules"
+    mkdir -p "$cache_dir/node/cache/node_modules"
+    cp -a "$build_dir/node_modules" "$(dirname "$cache_dir/node/cache/node_modules")"
   else
-    # node_modules
-    if [[ -e "$build_dir/node_modules" ]]; then
-      echo "- node_modules"
-      mkdir -p "$cache_dir/node/cache/node_modules"
-      cp -a "$build_dir/node_modules" "$(dirname "$cache_dir/node/cache/node_modules")"
-    else
-      # this can happen if there are no dependencies
-      echo "- node_modules (nothing to cache)"
-    fi
+    # this can happen if there are no dependencies
+    echo "- node_modules (nothing to cache)"
   fi
-}
-
-save_custom_cache_directories() {
-  local cache_directories
-  local build_dir=${1:-}
-  local cache_dir=${2:-}
-  # Parse the input string with multiple lines: "a\nb\nc" into an array
-  mapfile -t cache_directories <<< "$3"
-
-  echo "Saving ${#cache_directories[@]} cacheDirectories (package.json):"
-
-  for cachepath in "${cache_directories[@]}"; do
-    if [ -e "$build_dir/$cachepath" ]; then
-      echo "- $cachepath"
-      mkdir -p "$cache_dir/node/cache/$cachepath"
-      cp -a "$build_dir/$cachepath" "$(dirname "$cache_dir/node/cache/$cachepath")"
-    else
-      echo "- $cachepath (nothing to cache)"
-    fi
-  done
 }
